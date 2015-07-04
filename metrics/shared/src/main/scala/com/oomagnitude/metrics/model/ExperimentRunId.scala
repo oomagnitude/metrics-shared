c01491cc27047d169a5ccafe67f5673c2b78a8e9
yakticus <julie@yakticus.com>
2015-07-03 21:40:41 -0700

    tweaks

--- metrics/shared/src/main/scala/com/oomagnitude/metrics/model/ExperimentRunId.scala
+++ metrics/shared/src/main/scala/com/oomagnitude/metrics/model/ExperimentRunId.scala
@@ -1,17 +1,5 @@
 package com.oomagnitude.metrics.model
 
-import java.text.SimpleDateFormat
-import java.util.Date
-
-object ExperimentRunId {
-  val DirectoryDateFormat = new SimpleDateFormat("YYYY-MM-dd-HH-mm-ss")
-
-  def now(experimentId: ExperimentId): ExperimentRunId = {
-    val dateString = DirectoryDateFormat.format(new Date)
-    ExperimentRunId(experimentId, dateString)
-  }
-}
-
 case class ExperimentRunId(experimentId: ExperimentId, date: String) {
   val experiment = experimentId.experiment
 
--- metrics/shared/src/main/scala/com/oomagnitude/metrics/model/ModelImplicits.scala
+++ metrics/shared/src/main/scala/com/oomagnitude/metrics/model/ModelImplicits.scala
@@ -8,4 +8,24 @@ trait ModelImplicits {
   implicit val timeUnit2TimeUnit = TimeUnit.timeUnit2TimeUnit _
 
   implicit val jTimeUnit2TimeUnit = TimeUnit.jTimeUnit2TimeUnit _
+
+  implicit val experimentIdOrdering = new Ordering[ExperimentId] {
+    override def compare(x: ExperimentId, y: ExperimentId): Int = x.experiment.compare(y.experiment)
+  }
+
+  implicit val experimentRunOrdering = new Ordering[ExperimentRunId] {
+    override def compare(x: ExperimentRunId, y: ExperimentRunId): Int = y.date.compare(x.date)
+  }
+
+  implicit val metricIdOrdering = new Ordering[MetricId] {
+    override def compare(x: MetricId, y: MetricId): Int = x.toString.compare(y.toString)
+  }
+
+  implicit val dataSourceOrdering = new Ordering[DataSourceId] {
+    override def compare(x: DataSourceId, y: DataSourceId): Int = metricIdOrdering.compare(x.metricId, y.metricId)
+  }
+
+  implicit val metadataOrdering = new Ordering[MetricMetadata] {
+    override def compare(x: MetricMetadata, y: MetricMetadata): Int = dataSourceOrdering.compare(x.id, y.id)
+  }
 }
